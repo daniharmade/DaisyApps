b1e4736867c5d038a92c22fc85de67a72873fad1
Dani Harmade <12250310356@students.uin-suska.ac.id>
2024-11-30 20:00:51 +0700

    [ADD] Pup Up Delete & [Revisi] App Name

--- app/src/main/java/com/example/daisyapp/view/ui/history/HistoryFragment.kt
+++ app/src/main/java/com/example/daisyapp/view/ui/history/HistoryFragment.kt
@@ -1,17 +1,58 @@
 package com.example.daisyapp.view.ui.history
 
+import android.app.Dialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import com.example.daisyapp.R
+import com.example.daisyapp.databinding.FragmentHistoryBinding
 
 class HistoryFragment : Fragment() {
-        override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
-        ): View? {
-            return inflater.inflate(R.layout.fragment_history, container, false)
+    private var _binding: FragmentHistoryBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentHistoryBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnDelete.setOnClickListener {
+            showPopupDelete()
         }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    private fun showPopupDelete() {
+        val dialog = Dialog(requireContext())
+        dialog.setContentView(R.layout.pop_up_delete_dialog)
+        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+
+        val btnCancel = dialog.findViewById<Button>(R.id.btn_cancel)
+        val btnDelete = dialog.findViewById<Button>(R.id.btn_delete)
+
+        btnCancel.setOnClickListener {
+            dialog.dismiss()
+        }
+
+        btnDelete.setOnClickListener {
+//            viewModel.logout()
+            dialog.dismiss()
+        }
+        dialog.show()
+    }
 }
